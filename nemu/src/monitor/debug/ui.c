a8b0c28ed67a94879a0824e5cfa3459bddbc9a9e
tracer-ics2019 <tracer@njuics.org>
2019-11-16 17:28:34 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:28:34 up  6:26,  1 user,  load average: 0.51, 0.41, 0.26 389599b6282f7a0130be3d1870a0a4822622f33f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,13 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+/**************************************work******************************/
+//TODO1
+static int cmd_si(char* args){  //si N
+  int n=*(int*)args;
+  cpu_exec(n);
+  return 0;
+}
 static struct {
   char *name;
   char *description;
@@ -46,6 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Single instruction",cmd_si},
 
   /* TODO: Add more commands */
 
