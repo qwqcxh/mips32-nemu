09e1bae995eaf0fbfa8365fa175dceaa3476eea8
tracer-ics2019 <tracer@njuics.org>
2019-11-17 10:10:31 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:10:31 up 47 min,  1 user,  load average: 1.62, 0.79, 0.61 930914c92cb3383f37450dfd176c84bca22eb3aa

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -89,7 +89,7 @@ static int cmd_p(char* args){
   bool valid=true;
   uint32_t val=expr(args,&valid);
   if(valid==false) printf("invalid expression\n");
-  else printf("%-10x\n",val);
+  else printf("0x%010x\n",val);
   return 0;
 }
 
