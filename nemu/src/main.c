461b98a1ed9a45f8c6aac187b0a3fd9aa5fdb9b1
tracer-ics2019 <tracer@njuics.org>
2019-11-17 21:32:54 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:32:54 up 12:09,  1 user,  load average: 0.42, 0.33, 0.25 67100c5e284043824a446ceec7e965a7d2399f20

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,23 +12,23 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
 
   /***********work***************/
-  // FILE* fp=fopen("tools/gen-expr/input","r");
-  // Assert(fp!=NULL,"open input failed!\n");
-  // unsigned res;
-  // int line=1;
-  // while(fscanf(fp,"%u",&res)==1){
-  //   if(fgets(buf,65536,fp)==NULL)
-  //     panic("input file format error!\n");
-  //   int len=strlen(buf);
-  //   Assert(buf[len-1]=='\n',"fgets doesn't save '\'n character");
-  //   buf[strlen(buf)-1]='\0';
-  //   printf("buf is :%s:\n",buf);//debug
-  //   bool valid=true;
-  //   unsigned test_val=expr(buf,&valid);
-  //   Assert(valid,"expression is invalid!");
-  //   Assert(res==test_val,"expr function test failed at line:%d ,expected val %u but get %u\n",line,res,test_val);
-  //   line++;
-  // }
+  FILE* fp=fopen("tools/gen-expr/input","r");
+  Assert(fp!=NULL,"open input failed!\n");
+  unsigned res;
+  int line=1;
+  while(fscanf(fp,"%u",&res)==1){
+    if(fgets(buf,65536,fp)==NULL)
+      panic("input file format error!\n");
+    int len=strlen(buf);
+    Assert(buf[len-1]=='\n',"fgets doesn't save '\'n character");
+    buf[strlen(buf)-1]='\0';
+    printf("buf is :%s:\n",buf);//debug
+    bool valid=true;
+    unsigned test_val=expr(buf,&valid);
+    Assert(valid,"expression is invalid!");
+    Assert(res==test_val,"expr function test failed at line:%d ,expected val %u but get %u\n",line,res,test_val);
+    line++;
+  }
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
