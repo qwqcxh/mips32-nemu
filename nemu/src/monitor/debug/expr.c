f3734d86361c497c6a209cbb96c62494bd015140
tracer-ics2019 <tracer@njuics.org>
2019-11-17 23:13:07 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:13:07 up 25 min,  1 user,  load average: 0.43, 0.38, 0.42 f88433bde845286b10bd37aff62d3b56186d0f25

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,8 +204,8 @@ uint32_t eval(int p,int q,bool* success){
       uint32_t val;
       sscanf(tokens[p].str,"%u",&val);
       //debug
-      printf("str is %s and val is %u\n",tokens[p].str,val);
-      printf("now the val is digit num  %u\n",val); //debug
+      // printf("str is %s and val is %u\n",tokens[p].str,val);
+      // printf("now the val is digit num  %u\n",val); //debug
       return val;
     }else if(tokens[p].type==TK_HEX){
       uint32_t val;
@@ -242,16 +242,16 @@ uint32_t eval(int p,int q,bool* success){
       if(*success==false) return -1;
       switch (tokens[master_op_idx].type) {
         case '+': 
-          printf("now the val is + %u\n",val1+val2); //debug
+          //printf("now the val is + %u\n",val1+val2); //debug
           return val1 + val2;
         case '-': 
-          printf("now the val is - %u\n",val1-val2); //debug
+          //printf("now the val is - %u\n",val1-val2); //debug
           return val1 - val2;
         case '*': 
-          printf("now the val is * %u\n",val1*val2); //debug
+          //printf("now the val is * %u\n",val1*val2); //debug
           return val1 * val2;
         case '/': 
-          printf("now the val is / %u\n",val1/val2); //debug
+          //printf("now the val is / %u\n",val1/val2); //debug
           return val1 / val2;
         default: assert(0);return -1;
       }
