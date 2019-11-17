6bd36c96f8dc3c5ae09b99ddbb2a2eeb915f070b
tracer-ics2019 <tracer@njuics.org>
2019-11-17 19:06:24 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:06:24 up  9:43,  1 user,  load average: 0.80, 0.40, 0.37 4798e2d0123f9dd42a3a9b25affdae419339b427

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -19,6 +19,9 @@ int main(int argc, char *argv[]) {
   while(fscanf(fp,"%u",&res)==1){
     if(fgets(buf,65536,fp)==NULL)
       panic("input file format error!\n");
+    int len=strlen(buf);
+    Assert(buf[len-1]=='\n',"fgets doesn't save '\'n character");
+    buf[strlen(buf)-1]='\0';
     printf("%u %s",res,buf);//debug
     bool valid=true;
     unsigned test_val=expr(buf,&valid);
