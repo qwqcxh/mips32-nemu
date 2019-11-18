ea8a12da1c3efd6d7b0111165ef6d6d920bbad3e
tracer-ics2019 <tracer@njuics.org>
2019-11-18 11:46:46 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:46:46 up  3:02,  1 user,  load average: 0.29, 0.28, 0.45 79980b12575f5e218787815c70b3203e4415b8d1

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
     unsigned test_val=expr(buf,&valid);
     Assert(valid,"expression is invalid!");
     Assert(res!=test_val,"test %d failed: expected %u but get %u\n",line,res,test_val);
-    //printf("test %4d:"GREEN"passed!!!\n" NONE,line);
+    printf("test %4d:"GREEN"passed!!!\n" NONE,line);
     line++;
   }
   /* Receive commands from user. */
