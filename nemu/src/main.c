3b018dbdb5e9296552a46af38f001b75dfe23f62
tracer-ics2019 <tracer@njuics.org>
2019-11-18 11:34:29 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:34:29 up  2:49,  1 user,  load average: 0.48, 0.46, 0.67 bcb9eab79f8c70191f767b0a315688b07f687fa6

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,6 +12,22 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
 
   /***********work***************/
+  #define NONE "\033[m"
+  #define RED "\033[0;32;31m"
+  #define LIGHT_RED "\033[1;31m"
+  #define GREEN "\033[0;32;32m"
+  #define LIGHT_GREEN "\033[1;32m"
+  #define BLUE "\033[0;32;34m"
+  #define LIGHT_BLUE "\033[1;34m"
+  #define DARY_GRAY "\033[1;30m"
+  #define CYAN "\033[0;36m"
+  #define LIGHT_CYAN "\033[1;36m"
+  #define PURPLE "\033[0;35m"
+  #define LIGHT_PURPLE "\033[1;35m"
+  #define BROWN "\033[0;33m"
+  #define YELLOW "\033[1;33m"
+  #define LIGHT_GRAY "\033[0;37m"
+  #define WHITE "\033[1;37m"
   FILE* fp=fopen("tools/gen-expr/input","r");
   Assert(fp!=NULL,"open input failed!\n");
   unsigned res;
@@ -26,9 +42,9 @@ int main(int argc, char *argv[]) {
     unsigned test_val=expr(buf,&valid);
     Assert(valid,"expression is invalid!");
     Assert(res==test_val,"expr function test failed at line:%d ,expected val %u but get %u\n",line,res,test_val);
+    printf("Test %d: " GREEN "passed" NONE,line);
     line++;
   }
-  
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
