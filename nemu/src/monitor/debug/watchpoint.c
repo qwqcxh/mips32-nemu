378d6176f1027e2362f22b599188bde0c17eff32
tracer-ics2019 <tracer@njuics.org>
2019-11-23 11:28:36 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:28:36 up  1:54,  1 user,  load average: 0.25, 0.33, 0.22 5eb5230ab0dba3e55749d780c79f701bc2024613

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -70,10 +70,10 @@ void free_wp(int wp_num){
 }
 
 void wp_show(){
-  printf("%5s %30s\n","WP_NO","EXPRESSION");
+  printf("%5s %10s %30s\n","WP_NO","OLD_VAL","EXPRESSION");
   WP* pwp=head;
   while(pwp){
-    printf("%05d %s\n",pwp->NO,pwp->exp_str);
+    printf("%5d 0x%10x %30s\n",pwp->NO,pwp->old_val,pwp->exp_str);
     pwp=pwp->next;
   }
 }
