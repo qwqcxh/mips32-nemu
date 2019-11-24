b5bf82d17ccb711dbea76f7a0149afdcb2c04f8b
tracer-ics2019 <tracer@njuics.org>
2019-11-24 11:20:08 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:20:08 up  1:13,  1 user,  load average: 0.38, 0.37, 0.32 f9f6732e71e095b9bd5ce37dcc1968143d4602a

--- nemu/src/isa/mips32/exec/compute.c
+++ nemu/src/isa/mips32/exec/compute.c
@@ -4,10 +4,11 @@ make_EHelper(lui) {
   rtl_shli(&s0, &id_src2->val, 16);
   rtl_sr(id_dest->reg, &s0, 4);
 
-  print_asm_template3(lui);
+  print_asm_template2(lui);
 }
 
 /*******work***********/
 make_EHelper(or) {
   rtl_or(&reg_l(id_dest->reg),&id_src->val,&id_src2->val);
+  print_asm_template3(or);
 }
