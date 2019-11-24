0653a2e5b3fb153daacf9f65f7a483f5afa86ec3
tracer-ics2019 <tracer@njuics.org>
2019-11-24 16:30:29 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:30:29 up  6:23,  1 user,  load average: 0.86, 0.88, 0.71 ac7d51a4d878d650ad1effa8f57d3af76c5dc44

--- nemu/src/isa/mips32/exec/compute.c
+++ nemu/src/isa/mips32/exec/compute.c
@@ -4,17 +4,17 @@ make_EHelper(lui) {
   rtl_shli(&s0, &id_src2->val, 16);
   rtl_sr(id_dest->reg, &s0, 4);
 
-  print_asm_template3(lui);
+  print_asm("lui  %s,%s",id_dest->str,id_src2->str);
 }
 
 /*******work***********/
-make_EHelper(or) {
+make_EHelper(or) { //or rd,rs,rt
   rtl_or(&reg_l(id_dest->reg),&id_src->val,&id_src2->val);
-  print_asm_template3(or);
+  print_asm("or  %s,%s,%s",id_dest->str,id_src->str,id_src2->str);
 }
 
 make_EHelper(addiu){ //addiu rt,rs,imm  GPR[rt] <- GPR[rs] + sext[imm]
   rtl_sext(&s0,&id_src2->val,2);
   rtl_add(&reg_l(id_dest->reg),&id_src->val,&s0);
-  print_asm_template3(addiu);
+  print_asm("addiu  %s,%s,%d",id_dest->str,id_src->str,s0);
 }
