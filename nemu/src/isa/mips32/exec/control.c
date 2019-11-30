6f47d5f45d1d271ed2be884610ebba31255b2e54
tracer-ics2019 <tracer@njuics.org>
2019-11-30 23:31:40 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:31:40 up 13:39,  1 user,  load average: 1.81, 1.72, 0.83 3036e9b1acbe4805100a8a2c6eb9779d5e80fc4

--- nemu/src/isa/mips32/exec/control.c
+++ nemu/src/isa/mips32/exec/control.c
@@ -15,7 +15,7 @@ make_EHelper(jr){
 make_EHelper(beq){ //beq rs,rt,offset
     rtl_sext(&id_dest->val,&id_dest->val,2);
     rtl_shli(&id_dest->val,&id_dest->val,2);//get offset
-    rtl_addi(&id_dest->val,&id_dest->val,cpu.pc+8);
+    rtl_addi(&id_dest->val,&id_dest->val,cpu.pc+4);
     rtl_jrelop(RELOP_EQ,&id_src->val,&id_src2->val,id_dest->val);
     print_asm("beq %s,%s,0x%x",id_src->str,id_src2->str,id_dest->val);
 }
@@ -23,7 +23,7 @@ make_EHelper(beq){ //beq rs,rt,offset
 make_EHelper(bne){ //bne rs,rt,offset
     rtl_sext(&id_dest->val,&id_dest->val,2);
     rtl_shli(&id_dest->val,&id_dest->val,2);//get offset
-    rtl_addi(&id_dest->val,&id_dest->val,cpu.pc+8);
+    rtl_addi(&id_dest->val,&id_dest->val,cpu.pc+4);
     rtl_jrelop(RELOP_NE,&id_src->val,&id_src2->val,id_dest->val);
     print_asm("bne %s,%s,0x%x",id_src->str,id_src2->str,id_dest->val);
 }
