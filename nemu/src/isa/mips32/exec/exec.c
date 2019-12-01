c211c631da8a6fa1e7681edae507fe4679fa10a7
tracer-ics2019 <tracer@njuics.org>
2019-12-01 20:16:30 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-70-generic #79-Ubuntu SMP Tue Nov 12 10:36:11 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:16:30 up 10:49,  1 user,  load average: 1.54, 1.59, 1.14 d613f70a96c3efea3e9fda7b9f0f136e38cc5ddb

--- nemu/src/isa/mips32/exec/exec.c
+++ nemu/src/isa/mips32/exec/exec.c
@@ -6,7 +6,7 @@ static OpcodeEntry special_table [64] = {
   /* b001 */ IDEX(jr,jr), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
   /* b010 */ IDEX(mfhi,mfhi), EMPTY, IDEX(mflo,mflo), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
   /* b011 */ EMPTY, EMPTY, IDEX(div,div), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-  /* b100 */ EMPTY, IDEX(addu,addu), EMPTY, EMPTY, IDEX(and,and), IDEX(or,or), IDEX(xor,xor), IDEX(nor,nor),
+  /* b100 */ EMPTY, IDEX(addu,addu), EMPTY, IDEX(subu,subu), IDEX(and,and), IDEX(or,or), IDEX(xor,xor), IDEX(nor,nor),
   /* b101 */ EMPTY, EMPTY, IDEX(slt,slt), IDEX(sltu,sltu), EMPTY, EMPTY, EMPTY, EMPTY,
   /* b110 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
   /* b111 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
