8a658f46f790edf0d0f49205b9ebe2439f18376f
tracer-ics2019 <tracer@njuics.org>
2019-12-06 22:01:09 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:01:09 up 12:38,  1 user,  load average: 0.45, 0.25, 0.23 e270d0b141697e5e496c251f2b08c191e20473d8

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,83 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t ans=0;
+  for(const char* p=s;*p;p++) ans++;
+  return ans;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if(src==NULL) return dst=NULL;
+  for(size_t i=0;;i++){
+    dst[i]=src[i];
+    if(dst[i]=='\0') break;
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  if(src==NULL) return dst=NULL;
+  for(size_t i=0;i<n;i++){
+    dst[i]=src[i];
+    if(dst[i]=='\0') break;
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t dstlen=strlen(dst);
+  for(size_t i=0;;i++){
+    dst[dstlen+i]=src[i];
+    if(src[i]=='\0') break;
+  }
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while(s1&&s2){
+    if(*s1>*s2) return 1;
+    else if(*s1<*s2) return -1;
+    s1++;
+    s2++;
+  }
+  if(s1) return 1;
+  else if(s2) return -1;
+  else return 0; 
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  while(s1&&s2&&n){
+    if(*s1>*s2) return 1;
+    else if(*s1<*s2) return -1;
+    s1++;
+    s2++;
+    n--;
+  }
+  if(n==0) return 0;
+  else if(s1) return 1;
+  else if(s2) return -1;
+  else return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  for(size_t i=0;i<n;i++)
+    ((char*)v)[i]=c;
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  for(size_t i=0;i<n;i++)
+    ((char*)out)[i]=((char*)in)[i];
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  char* p=(char*)s1;
+  char* q=(char*)s2;
+  for(size_t i=0;i<n;i++){
+    if(p[i]>q[i]) return 1;
+    else if(p[i]<q[i]) return -1;
+  }
   return 0;
 }
 
