7309a4692f166738f54bcd58962ff7d3d0a9186f
tracer-ics2019 <tracer@njuics.org>
2019-12-07 21:28:25 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:28:25 up 1 day, 12:05,  1 user,  load average: 0.25, 0.43, 0.55 226fddc91c0f56b447989e5b263e2b8b55ea9109

--- nemu/src/isa/mips32/diff-test.c
+++ nemu/src/isa/mips32/diff-test.c
@@ -6,7 +6,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   uint32_t opcode=decinfo.isa.instr.opcode;
   uint32_t func=decinfo.isa.instr.func;
   if((opcode>=2&&opcode<=7) || (func>=8&&func<=9)){
-    difftest_skip_dut(2,1);
+    difftest_skip_dut(1,2);
     return true;
   }
   for(int i=0;i<32;i++){
