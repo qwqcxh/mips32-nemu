cb92c11a81712f5fcf778becb53a65b1883729f1
tracer-ics2019 <tracer@njuics.org>
2019-12-07 21:55:18 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:55:18 up 1 day, 12:32,  1 user,  load average: 0.57, 0.52, 0.51 45915be1360b524192565d5c959d9bd38810415

--- nemu/src/isa/mips32/diff-test.c
+++ nemu/src/isa/mips32/diff-test.c
@@ -3,8 +3,10 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;i++){
-    if(ref_r->gpr[i]._32!=cpu.gpr[i]._32)
+    if(ref_r->gpr[i]._32!=cpu.gpr[i]._32){
+      printf("%s: qemu 0x%x nemu 0x%x\n",reg_name(i,4),ref_r->gpr[i]._32,cpu.gpr[i]._32);
       return false;
+    }
   }
   return true;
 }
