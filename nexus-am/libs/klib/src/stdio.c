e18a9d65b69854455925d38b3b2de2197afd5d38
tracer-ics2019 <tracer@njuics.org>
2019-12-07 15:45:58 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:45:58 up 1 day,  6:23,  1 user,  load average: 0.34, 0.48, 0.39 97cb95c2d8c5ae55ed322d1bbfab8ca928cb95d7

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -7,16 +7,77 @@ int printf(const char *fmt, ...) {
   return 0;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//   return 0;
+// }
 
-int sprintf(char *out, const char *fmt, ...) {
-  return 0;
-}
+// int sprintf(char *out, const char *fmt, ...) {
+//   return 0;
+// }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
+}
+
+
+#define is_digit(x) ((x)>='0' && (x) <= '9')
+static char* number(char* str,unsigned long num,int base){
+  char tmp[100];
+  int i=0;
+  while(num){
+    tmp[i]=num%base;
+    num/=base;
+    if(tmp[i]<10) tmp[i]+='0';
+    else tmp[i]=tmp[i]-10+'a';
+    i++;
+  }
+  while(--i){
+    *str++=tmp[i];
+  }
+  return str;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap){
+  char *str;
+  for (str = out; *fmt; fmt++){
+    int base = 10; //default base if 10
+    if (*fmt != '%'){ //regular character
+      *str++ = *fmt;
+      continue;
+    }
+
+    /* Default base */
+    base = 10;
+    unsigned long num=-1;
+    switch (*fmt){
+      case 's':{
+        int len;
+        char * s = va_arg(ap, char *);
+        if (!s) s = "<NULL>";
+        len = strlen(s);
+        for (int i = 0; i < len; ++i) *str++ = *s++;
+        continue;
+      }
+      case 'd':{
+        num=(unsigned long)va_arg(ap,int);
+      }
+    }
+    str = number(str, num, base);
+  }
+  *str = '\0';
+  return str - out;
+}
+
+int sprintf(char *out, const char *fmt, ...)
+{
+    va_list args;
+    int n;
+
+    va_start(args, fmt);
+    n = vsprintf(out, fmt, args);
+    va_end(args);
+
+    return n;
 }
 
 #endif
