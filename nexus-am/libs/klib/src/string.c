d2f8c3c2483d93bd4a2f15de936dd6e9d02375e1
tracer-ics2019 <tracer@njuics.org>
2019-12-07 10:36:46 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:36:46 up 1 day,  1:14,  1 user,  load average: 0.58, 0.64, 0.64 dad83132125f9a87c6eec47596d6532bca39d4a5

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -42,8 +42,8 @@ int strcmp(const char* s1, const char* s2) {
     s1++;
     s2++;
   }
-  if(s1) return 1;
-  else if(s2) return -1;
+  if(*s1) return 1;
+  else if(*s2) return -1;
   else return 0; 
 }
 
@@ -56,8 +56,8 @@ int strncmp(const char* s1, const char* s2, size_t n) {
     n--;
   }
   if(n==0) return 0;
-  else if(s1) return 1;
-  else if(s2) return -1;
+  else if(*s1) return 1;
+  else if(*s2) return -1;
   else return 0;
 }
 
