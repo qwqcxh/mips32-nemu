7cd177ecf24c8d5ffcaa3359f0b5a7713460bf34
tracer-ics2019 <tracer@njuics.org>
2019-12-07 21:24:56 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:24:56 up 1 day, 12:02,  1 user,  load average: 0.55, 0.62, 0.63 bd08ea93ba5b189099e6ecc7a3a125dc643c65d7

--- nemu/src/isa/mips32/diff-test.c
+++ nemu/src/isa/mips32/diff-test.c
@@ -1,7 +1,14 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
-
+#include "cpu/decode.h"
+extern DecodeInfo decinfo;
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  uint32_t opcode=decinfo.isa.instr.opcode;
+  uint32_t func=decinfo.isa.instr.func;
+  if((opcode>=2&&opcode<=7) || (func>=8&&func<=9)){
+    difftest_skip_dut(2,1);
+    return true;
+  }
   for(int i=0;i<32;i++){
     if(ref_r->gpr[i]._32!=cpu.gpr[i]._32)
       return false;
