e706d261ed61a3c11516dfe0781710947bd87982
tracer-ics2019 <tracer@njuics.org>
2019-12-08 20:06:32 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:06:32 up 10:16,  1 user,  load average: 0.67, 0.46, 0.34 2e5c381c1f52c6ab8e2e33194a852d0d22d86690

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -6,8 +6,8 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      uptime->hi = 0;
-      uptime->lo = 0;
+      uptime->hi = inl((uintptr_t)(buf+4));
+      uptime->lo = inl((uintptr_t)buf);
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
