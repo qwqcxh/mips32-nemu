0d32be62ff993cebe05f208ff0fa3856b069c580
tracer-ics2019 <tracer@njuics.org>
2019-12-09 20:58:18 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:58:18 up 1 day, 11:08,  1 user,  load average: 0.47, 0.55, 0.53 7b9cca15663de13e1fce6a3e9b979cbb89ac5455

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -31,7 +31,8 @@ static inline void update_screen() {
 static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   // TODO: call `update_screen()` when writing to the sync register
   /**********work****************/
-  if(offset==4&&len==4&&is_write){
+  // if(offset==4&&len==4&&is_write){
+  if(is_write){
     update_screen();
   }
 }
