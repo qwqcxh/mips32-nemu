0873b7bdf69a8b64c20984ed9eca62f239398ae2
tracer-ics2019 <tracer@njuics.org>
2019-12-09 17:28:30 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:28:30 up 1 day,  7:38,  1 user,  load average: 1.43, 0.99, 0.72 d43906cd87da372c7c1cf919f5d477ded385745

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,12 +1,12 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG
-#define DIFF_TEST
+//#define DEBUG
+//#define DIFF_TEST
 
 #if _SHARE
 // do not enable these features while building a reference design
-//#undef DIFF_TEST
+#undef DIFF_TEST
 #undef DEBUG
 #endif
 
