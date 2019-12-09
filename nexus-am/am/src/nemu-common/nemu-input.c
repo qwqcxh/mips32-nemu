545314a6ff3045c109714eda9f0cc42b2747183e
tracer-ics2019 <tracer@njuics.org>
2019-12-09 19:42:13 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:42:13 up 1 day,  9:52,  1 user,  load average: 0.84, 0.59, 0.39 cf0c256c554a06c9ca7e799d47302d3dfb645038

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,9 +8,11 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      uint32_t k=inl(KBD_ADDR);
-      kbd->keydown = (k & KEYDOWN_MASK)?1:0;
-      kbd->keycode = k&(~KEYDOWN_MASK);
+      // uint32_t k=inl(KBD_ADDR);
+      // kbd->keydown = (k & KEYDOWN_MASK)?1:0;
+      // kbd->keycode = k&(~KEYDOWN_MASK);
+      kbd->keydown=0;
+      kbd->keycode=_KEY_NONE;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
