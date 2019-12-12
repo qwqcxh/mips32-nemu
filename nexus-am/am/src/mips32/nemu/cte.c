8e95e410feb50148d48ab11351b9765a44b15646
tracer-ics2019 <tracer@njuics.org>
2019-12-12 21:40:48 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:40:48 up 1 day,  2:18,  1 user,  load average: 0.93, 0.51, 0.40 91c719f49060232c51d05caccd86991b82b0c3b5

--- nexus-am/am/src/mips32/nemu/cte.c
+++ nexus-am/am/src/mips32/nemu/cte.c
@@ -14,7 +14,7 @@ _Context* __am_irq_handle(_Context *c) {
     _Event ev = {0};
     uint32_t ex_code = (c->cause>>2)&0x1f;
     switch (ex_code) {
-      case 8: ev.event = _EVENT_YIELD; break; //not correct completely, solve it later
+      case 13: ev.event = _EVENT_YIELD; break; //not correct completely, solve it later
       default: ev.event = _EVENT_ERROR; break;
     }
 
