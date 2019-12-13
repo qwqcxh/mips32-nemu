8f6d0d0e854c52d43933eb6b15b21724ff940108
tracer-ics2019 <tracer@njuics.org>
2019-12-13 20:34:18 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:34:18 up 10:32,  1 user,  load average: 0.30, 0.50, 0.81 d2efcdf2d2fa47cf4e7d27037c8a8189cdc3981

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -8,30 +8,30 @@ int main() {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
+  printf("test1 passed\n");//debug
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("test2 passed\n");//debug
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-
+  printf("test3 passed\n");//debug
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("test4 passed\n");//debug
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-
+  printf("test5 passed\n");//debug
   fclose(fp);
 
   printf("PASS!!!\n");
