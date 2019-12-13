b0bfa19e284ce7b5beeb8b27e97b055ca6a71a67
tracer-ics2019 <tracer@njuics.org>
2019-12-13 10:34:33 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:34:33 up 32 min,  1 user,  load average: 0.35, 0.32, 0.49 824c594a35e8b77cbf1685b5986e81d939bb8f11

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,18 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
     case SYS_yield: _yield();c->GPRx=0;break;
+    case SYS_write:{
+      int fd=c->GPR2;
+      char* p=(char*)c->GPR3;
+      size_t count = c->GPR4;
+      if(fd==1 || fd == 0){
+        for(int i=0;i<count;i++)
+          _putc(p[i]);
+        c->GPRx=count;
+      }
+      else c->GPRx=0;
+      break;
+    }
     case SYS_exit: _halt(c->GPR2); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
