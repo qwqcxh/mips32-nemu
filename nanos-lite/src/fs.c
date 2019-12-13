1e2ad6a8ee994df3e3f6a8f4bb1b44269612fe63
tracer-ics2019 <tracer@njuics.org>
2019-12-13 20:45:58 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:45:58 up 10:44,  1 user,  load average: 0.79, 0.63, 0.67 625535d6dda24011aebd20abac0f11d65d046e68

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -72,6 +72,5 @@ size_t fs_lseek(int fd, size_t offset, int whence){
   if(whence == SEEK_SET ) file_table[fd].open_offset = offset;
   else if(whence == SEEK_CUR) file_table[fd].open_offset+=offset;
   else file_table[fd].open_offset=file_table[fd].size+offset;
-  printf("lseek location is %d\n",file_table[fd].open_offset);//debug
   return file_table[fd].open_offset;
 }
--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -2,38 +2,30 @@
 #include <assert.h>
 
 int main() {
-  printf("test-1 passed\n"); //debug
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
-  printf("test0 passed\n"); //debug
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  printf("size if %d\n",size);
   assert(size == 5000);
-  printf("test1 passed\n");//debug
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("test2 passed\n");//debug
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  printf("test3 passed\n");//debug
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("test4 passed\n");//debug
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-  printf("test5 passed\n");//debug
   fclose(fp);
 
   printf("PASS!!!\n");
