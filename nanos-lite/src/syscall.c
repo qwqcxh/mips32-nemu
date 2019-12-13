487f1254caadd5b43da1ac3859018dea8979c0c0
tracer-ics2019 <tracer@njuics.org>
2019-12-13 20:43:24 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:43:24 up 10:41,  1 user,  load average: 0.89, 0.47, 0.64 9dd9a16be28e2a1dbc3b47201ab54dac43d9e7a7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,6 +20,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_open:c->GPRx=fs_open((char*)c->GPR2,c->GPR3,c->GPR4);break;
     case SYS_read:c->GPRx=fs_read(c->GPR2,(void*)c->GPR3,c->GPR4);break;
     case SYS_close:c->GPRx=fs_close(c->GPR2);break;
+    case SYS_lseek:c->GPRx=fs_lseek(c->GPR2,c->GPR3,c->GPR4);break;
     case SYS_write:{
       int fd=c->GPR2;
       char* p=(char*)c->GPR3;
