aed943d3aca7206e37d0ea5cbd2ea3fb72b6df2d
tracer-ics2019 <tracer@njuics.org>
2019-12-14 20:05:53 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:05:53 up 1 day, 10:04,  1 user,  load average: 0.54, 0.28, 0.34 fea68c758378fe14660a984134a47c9995d460d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -43,7 +43,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int width  = screen_width();
   int height = screen_height();
   offset/=4;
-  int x = offset % (width-1);
+  int x = offset % (width);
   int y = offset / width;
   printf("x y w h len offset %d %d %d %d %d %u\n",x,y,width,height,len,offset);//debug
   draw_rect((uint32_t*)buf,x,y,4,4);
