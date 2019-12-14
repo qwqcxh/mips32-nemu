2539b4885c4d38e2fe615685dd6166af89d3aabe
tracer-ics2019 <tracer@njuics.org>
2019-12-14 19:01:08 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:01:08 up 1 day,  8:59,  1 user,  load average: 0.55, 0.43, 0.39 f85eae9393c6169c9b3a20406ba09f8f17c05e95

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,7 +61,7 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t fs_read(int fd, void *buf, size_t len){
   size_t real_off=file_table[fd].disk_offset + file_table[fd].open_offset;
   size_t ret;
-  if(file_table[fd].read) ret = file_table[fd].read(buf,0,len);
+  if(file_table[fd].read) ret = file_table[fd].read(buf,real_off,len);
   else ret = ramdisk_read(buf, real_off, len);
   file_table[fd].open_offset+=ret;
   return ret;
@@ -70,10 +70,11 @@ size_t fs_read(int fd, void *buf, size_t len){
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len){
   size_t real_off=file_table[fd].disk_offset + file_table[fd].open_offset;
-  if(file_table[fd].write) file_table[fd].write(buf,0,len);
-  else ramdisk_write(buf, real_off, len); 
-  file_table[fd].open_offset+=len;
-  return len;
+  size_t ret;
+  if(file_table[fd].write) ret = file_table[fd].write(buf,real_off,len);
+  else ret = ramdisk_write(buf, real_off, len); 
+  file_table[fd].open_offset+=ret;
+  return ret;
 }
 
 int fs_close(int fd){
