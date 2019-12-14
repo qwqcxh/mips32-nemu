6d89dac86cca5f6a94e975ead5c8b63412e42328
tracer-ics2019 <tracer@njuics.org>
2019-12-14 19:24:02 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:24:02 up 1 day,  9:22,  1 user,  load average: 0.49, 0.41, 0.41 776e84b52a00c7359a2c20973bc5de868ad270d

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,6 +50,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  printf("draw sync now\n");//debug
   draw_sync();
   return len;
 }
