fa2f2bac3bcd4f645f5f80b334758d293654a045
tracer-ics2019 <tracer@njuics.org>
2019-12-14 18:31:01 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:31:01 up 1 day,  8:29,  1 user,  load average: 0.31, 0.27, 0.35 70d3156dd7bc2edf159285cab4a6e2cb5f20427

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,6 +31,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  printf("dispinfo read offset len : %u %u\n",offset,len);//debug
   int tail = strlen(dispinfo);
   if(offset >= tail) return 0;
   int ret = len < tail - offset ? len : tail - offset;
