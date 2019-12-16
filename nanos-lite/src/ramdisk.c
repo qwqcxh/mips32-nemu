cffadfff740202e3a6815dfaa47571cd59303057
tracer-ics2019 <tracer@njuics.org>
2019-12-16 11:44:51 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:44:51 up 3 days,  1:43,  1 user,  load average: 0.69, 0.61, 0.86 47744bd59ff491e7a787e210adfab4a01eab2434

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -17,7 +17,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   if(offset == 29019871){
     printf("debug here and last val %x and &ramdis_start+offset is %x\n",*(uint32_t*)(buf+len-8),&ramdisk_start+offset);
     for(int k=0;k<len;k+=4)
-      printf("%x ",(*(uint32_t*)(buf+k*4)));
+      printf("[%x %x] ",(*(uint32_t*)(buf+k*4)),((uint32_t*)(&ramdisk_start+offset))[k]);
   }
   return len;
 }
