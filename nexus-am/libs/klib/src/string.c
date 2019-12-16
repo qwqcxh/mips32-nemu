332c76f371c989e92bab018e9025e51da7b8c7f7
tracer-ics2019 <tracer@njuics.org>
2019-12-16 19:11:07 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:11:07 up 3 days,  9:09,  1 user,  load average: 0.41, 0.36, 0.22 72962546fe7f9b2d75d813cc304eb9e633e3272

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -67,14 +67,64 @@ void* memset(void* v,int c,size_t n) {
   return v;
 }
 
+/* Nonzero if either X or Y is not aligned on a "long" boundary.  */
+#define UNALIGNED(X, Y) \
+  (((long)X & (sizeof (long) - 1)) | ((long)Y & (sizeof (long) - 1)))
+
+/* How many bytes are copied each iteration of the 4X unrolled loop.  */
+#define BIGBLOCKSIZE    (sizeof (long) << 2)
+
+/* How many bytes are copied each iteration of the word copy loop.  */
+#define LITTLEBLOCKSIZE (sizeof (long))
+
+/* Threshhold for punting to the byte copier.  */
+#define TOO_SMALL(LEN)  ((LEN) < BIGBLOCKSIZE)
 void* memcpy(void* out, const void* in, size_t n) {
-  for(size_t i=0;i<n;i++)
-    ((char*)out)[i]=((char*)in)[i];
-  for(size_t i=0;i<n;i++)
-    if(i%4==0){
-      printf("i %d memcpy debug: %x %x\n",i,((uint32_t*)in)[i/4],((uint32_t*)out)[i/4]);
+  char *dst = (char*)out;
+  const char *src = (char*)in;
+  long *aligned_dst;
+  const long *aligned_src;
+
+  /* If the size is small, or either SRC or DST is unaligned,
+     then punt into the byte copy loop.  This should be rare.  */
+  if (!TOO_SMALL(n) && !UNALIGNED (src, dst))
+    {
+      aligned_dst = (long*)dst;
+      aligned_src = (long*)src;
+
+      /* Copy 4X long words at a time if possible.  */
+      while (n >= BIGBLOCKSIZE)
+        {
+          *aligned_dst++ = *aligned_src++;
+          *aligned_dst++ = *aligned_src++;
+          *aligned_dst++ = *aligned_src++;
+          *aligned_dst++ = *aligned_src++;
+          n -= BIGBLOCKSIZE;
+        }
+
+      /* Copy one long word at a time if possible.  */
+      while (n >= LITTLEBLOCKSIZE)
+        {
+          *aligned_dst++ = *aligned_src++;
+          n -= LITTLEBLOCKSIZE;
+        }
+
+       /* Pick up any residual with a byte copier.  */
+      dst = (char*)aligned_dst;
+      src = (char*)aligned_src;
     }
-  return out;
+
+  while (n--)
+    *dst++ = *src++;
+
+  return dst;
+  // for(size_t i=0;i<n;i++)
+  //   ((char*)out)[i]=((char*)in)[i];
+  // for(size_t i=0;i<n;i++)
+  //   if(i%4==0){
+  //     printf("i %d memcpy debug: %x %x\n",i,((uint32_t*)in)[i/4],((uint32_t*)out)[i/4]);
+  //   }
+  // return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
