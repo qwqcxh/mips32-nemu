3935d249b272ca3581264ce5b5cd73233f10ca8d
tracer-ics2019 <tracer@njuics.org>
2019-12-16 20:33:59 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:33:59 up 3 days, 10:32,  1 user,  load average: 0.49, 0.33, 0.40 7b01af91ebb1356c1a04a2f726dd0a2c71853ac

--- nexus-am/tests/cputest/tests/string.c
+++ nexus-am/tests/cputest/tests/string.c
@@ -40,8 +40,8 @@ char *s[] = {
 char str1[] = "Hello";
 char str[20];
 
-char cpy1[] = "abcdefghigklmnopqrstuvwxyzaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-char cpy2[100];
+unsigned cpy1[] ={0xbaaa73b7,0xc6a847ac,0xc2c22020};
+unsigned cpy2[100];
 int main() {
 	// nemu_assert(strcmp(s[0], s[2]) == 0);
 	// nemu_assert(strcmp(s[0], s[1]) == -1);
@@ -52,7 +52,7 @@ int main() {
 	// nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
 	// nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
-	memcpy(cpy2,cpy1+5,100);
-	printf("%s\n",cpy2);
+	memcpy(cpy2,cpy1,10);
+	for(int i=0;i<10;i++) printf("%x ",((char*)cpy2)[i]);
 	return 0;
 }
