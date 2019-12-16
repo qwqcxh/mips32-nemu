e43da7d9df59284086477edafdee238b4af8b9e5
tracer-ics2019 <tracer@njuics.org>
2019-12-16 14:12:09 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:12:09 up 3 days,  4:10,  1 user,  load average: 0.26, 0.23, 0.17 404cfb7bea7c850a56c2a2edd613af4da1d4ded

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -14,9 +14,9 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   //printf("offset is %u and len is %u\n",offset,len);//debug
   assert(offset + len <= RAMDISK_SIZE);
   //memcpy(buf, &ramdisk_start + offset, len);
-  char* q = (char*)buf;
-  char* p = (char*)(&ramdisk_start+offset);
-  for(int i=0;i<len;i++) q[i]=p[i];//debug
+  uint32_t* q = (uint32_t*)buf;
+  uint32_t* p = (uint32_t*)(&ramdisk_start+offset);
+  for(int i=0;i<len/4;i++) q[i]=p[i];//debug
   if(offset == 29019871){
     printf("debug here and last val %x and &ramdis_start+offset is %x\n",*(uint32_t*)(buf+len-4),&ramdisk_start+offset);
     for(int k=0;k<len;k+=4)
