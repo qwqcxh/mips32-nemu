6ab356eac95c13e287f1bdc214ee85095cce0c47
tracer-ics2019 <tracer@njuics.org>
2019-12-16 15:37:58 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:37:58 up 3 days,  5:36,  1 user,  load average: 0.48, 0.33, 0.30 b9284009899b19c8d4cad70e1e34221bbcd47f7

--- navy-apps/libs/libc/src/string/memcpy.c
+++ navy-apps/libs/libc/src/string/memcpy.c
@@ -50,56 +50,60 @@ memcpy (void *__restrict dst0,
 	const void *__restrict src0,
 	size_t len0)
 {
-#if defined(PREFER_SIZE_OVER_SPEED) || defined(__OPTIMIZE_SIZE__)
-  char *dst = (char *) dst0;
-  char *src = (char *) src0;
+  char* dst = (char*)dst0;
+  char* src = (char*)src0;
+  for(size_t i=0;i<len0;i++) dst[i]=src[i];
+  return dst; 
+// #if defined(PREFER_SIZE_OVER_SPEED) || defined(__OPTIMIZE_SIZE__)
+//   char *dst = (char *) dst0;
+//   char *src = (char *) src0;
 
-  void *save = dst0;
+//   void *save = dst0;
 
-  while (len0--)
-    {
-      *dst++ = *src++;
-    }
+//   while (len0--)
+//     {
+//       *dst++ = *src++;
+//     }
 
-  return save;
-#else
-  char *dst = dst0;
-  const char *src = src0;
-  long *aligned_dst;
-  const long *aligned_src;
+//   return save;
+// #else
+//   char *dst = dst0;
+//   const char *src = src0;
+//   long *aligned_dst;
+//   const long *aligned_src;
 
-  /* If the size is small, or either SRC or DST is unaligned,
-     then punt into the byte copy loop.  This should be rare.  */
-  if (!TOO_SMALL(len0) && !UNALIGNED (src, dst))
-    {
-      aligned_dst = (long*)dst;
-      aligned_src = (long*)src;
+//   /* If the size is small, or either SRC or DST is unaligned,
+//      then punt into the byte copy loop.  This should be rare.  */
+//   if (!TOO_SMALL(len0) && !UNALIGNED (src, dst))
+//     {
+//       aligned_dst = (long*)dst;
+//       aligned_src = (long*)src;
 
-      /* Copy 4X long words at a time if possible.  */
-      while (len0 >= BIGBLOCKSIZE)
-        {
-          *aligned_dst++ = *aligned_src++;
-          *aligned_dst++ = *aligned_src++;
-          *aligned_dst++ = *aligned_src++;
-          *aligned_dst++ = *aligned_src++;
-          len0 -= BIGBLOCKSIZE;
-        }
+//       /* Copy 4X long words at a time if possible.  */
+//       while (len0 >= BIGBLOCKSIZE)
+//         {
+//           *aligned_dst++ = *aligned_src++;
+//           *aligned_dst++ = *aligned_src++;
+//           *aligned_dst++ = *aligned_src++;
+//           *aligned_dst++ = *aligned_src++;
+//           len0 -= BIGBLOCKSIZE;
+//         }
 
-      /* Copy one long word at a time if possible.  */
-      while (len0 >= LITTLEBLOCKSIZE)
-        {
-          *aligned_dst++ = *aligned_src++;
-          len0 -= LITTLEBLOCKSIZE;
-        }
+//       /* Copy one long word at a time if possible.  */
+//       while (len0 >= LITTLEBLOCKSIZE)
+//         {
+//           *aligned_dst++ = *aligned_src++;
+//           len0 -= LITTLEBLOCKSIZE;
+//         }
 
-       /* Pick up any residual with a byte copier.  */
-      dst = (char*)aligned_dst;
-      src = (char*)aligned_src;
-    }
+//        /* Pick up any residual with a byte copier.  */
+//       dst = (char*)aligned_dst;
+//       src = (char*)aligned_src;
+//     }
 
-  while (len0--)
-    *dst++ = *src++;
+//   while (len0--)
+//     *dst++ = *src++;
 
-  return dst0;
-#endif /* not PREFER_SIZE_OVER_SPEED */
+//   return dst0;
+// #endif /* not PREFER_SIZE_OVER_SPEED */
 }
