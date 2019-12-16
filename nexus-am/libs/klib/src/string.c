bb62d6d1b834f507198dfc0e22c9917a59dbca2a
tracer-ics2019 <tracer@njuics.org>
2019-12-16 14:28:40 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:28:40 up 3 days,  4:26,  1 user,  load average: 0.97, 0.40, 0.28 fe297240524032896a77786a4cc2407cc9f19ff5

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -70,14 +70,8 @@ void* memset(void* v,int c,size_t n) {
 void* memcpy(void* out, const void* in, size_t n) {
   char* p = (char*)in;
   char* q = (char*)out;
-  for(size_t i=0;i<n;i++){    
+  for(size_t i=0;i<n;i++)    
     q[i]=p[i];
-    if((uint32_t)in ==  0x81cb048f){ //debug
-      // printf("n is %u  %x %x\n",n,((char*)p)[i],((char*)q)[i]);
-      // printf("output by word %x %x out addr %x\n",((uint32_t*)p)[i],((uint32_t*)q)[i],out);
-    }
-  }
-    // ((char*)out)[i]=((char*)in)[i];
   return out;
 }
 
