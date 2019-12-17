bedeb0eeae4704d03a133d4263dd82957b638709
tracer-ics2019 <tracer@njuics.org>
2019-12-17 20:13:15 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:13:15 up  7:22,  1 user,  load average: 0.20, 0.20, 0.18 435882725db1ebc16dd7c5d37ab4f4d168783ed

--- nexus-am/tests/cputest/tests/unalign.c
+++ nexus-am/tests/cputest/tests/unalign.c
@@ -6,11 +6,11 @@ volatile unsigned char buf[16];
 int main() {
 
 	for(int i = 0; i < 4; i++) {
-		*((volatile unsigned*)(buf + 3)) = 0xaabbccdd;
-		*((volatile unsigned*)(buf + 7)) = 0x66778899;
+		*((volatile unsigned*)(buf + 1)) = 0xaabbccdd;
+		*((volatile unsigned*)(buf + 3)) = 0x66778899;
 
-		x = *((volatile unsigned*)(buf + 3));
-		nemu_assert(x == 0xaabbccdd);
+		x = *((volatile unsigned*)(buf + 1));
+		nemu_assert(x == 0x8899ccdd);
 		x = *((volatile unsigned*)(buf + 7));
 		nemu_assert(x == 0x66778899);
 		buf[0] = buf[1] = 0;
