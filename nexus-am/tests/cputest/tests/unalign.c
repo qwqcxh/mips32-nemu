6416188b55c00f1a89fe047f1a532640b522f3f2
tracer-ics2019 <tracer@njuics.org>
2019-12-17 19:15:27 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:15:27 up  6:24,  1 user,  load average: 0.43, 0.26, 0.25 1a36352e2ac58aafe885aefe10326bc762ce0a63

--- nexus-am/tests/cputest/tests/unalign.c
+++ nexus-am/tests/cputest/tests/unalign.c
@@ -2,13 +2,15 @@
 
 volatile unsigned x = 0xffffffff;
 volatile unsigned char buf[16];
+unsigned char dst[20]={0xff,0xff,0xff,0xff,0xff,0xff,0xff};
 
 int main() {
 
 	for(int i = 0; i < 4; i++) {
 		*((volatile unsigned*)(buf + 3)) = 0xaabbccdd;
 
-		x = *((volatile unsigned*)(buf + 3));
+		x= *(volatile unsigned*)(dst+3) = *((volatile unsigned*)(buf + 3));
+		for(int i=0;i<7;i++) printf("%x ",dst[i]);
 		nemu_assert(x == 0xaabbccdd);
 
 		buf[0] = buf[1] = 0;
