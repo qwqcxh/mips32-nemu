7616d5b118799d4f50eb6413e4e06e17f9074d21
tracer-ics2019 <tracer@njuics.org>
2019-12-17 14:15:54 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:15:54 up  1:25,  1 user,  load average: 0.98, 0.69, 0.37 277a71c1c094d12d701ce8f4aa92b07c8fdf8723

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -95,7 +95,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 
 void difftest_step(vaddr_t ori_pc, vaddr_t next_pc) {
   CPU_state ref_r;
-  if((uint32_t)ori_pc == 0x83027ef4) { //debug
+  if((uint32_t)ori_pc == 0x83027ef8) { //debug
     //difftest_attach();
     ref_difftest_setregs(&cpu);
     is_detach =  false;
