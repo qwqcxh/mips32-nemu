6952fdb2f751ba60906dbbb021a4f6fce3bbeb69
tracer-ics2019 <tracer@njuics.org>
2019-12-19 21:16:21 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:16:21 up 12:07,  1 user,  load average: 0.74, 0.44, 0.36 cf2faf0687e36577df1cf9cddf80ac87338f81da

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c);break;
-    case _EVENT_YIELD: c = schedule(c);printf("yield succeed!\n");break;
+    case _EVENT_YIELD: c = schedule(c);printf("yield succeed! new context %x\n",c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nemu/src/isa/mips32/exec/system.c
+++ nemu/src/isa/mips32/exec/system.c
@@ -39,7 +39,7 @@ make_EHelper(mtc0){
 make_EHelper(cop0_func){
     switch(decinfo.isa.instr.func){
         case 0x18: //ERET
-            assert(cpu.epc != 0x70206772); //debug
+            // assert(cpu.epc != 0x70206772); //debug
             rtl_andi(&cpu.status,&cpu.status,0xfffffffd);
             rtl_j(cpu.epc+4);
             break;
