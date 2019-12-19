e702948602572d2623747844cf238930bcab84ca
tracer-ics2019 <tracer@njuics.org>
2019-12-19 22:15:41 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:15:41 up 13:06,  1 user,  load average: 0.08, 0.10, 0.27 e40496745ea88c57cb38e967effe72cdee05c7d0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,7 +35,7 @@ void init_proc() {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   printf("current is %x and current->cp is %x\n",current,current->cp);//debug
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
