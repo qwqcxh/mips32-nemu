45023f1ad4b971cfff47173a3bba229299b4a3ab
tracer-ics2019 <tracer@njuics.org>
2019-12-22 16:42:26 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:42:26 up  7:00,  1 user,  load average: 0.22, 0.20, 0.25 b4d431d37a7bafb05f2d09f5ed557b31200e161b

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,11 +3,12 @@
 extern _Context* do_syscall(_Context *c);
 extern _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
+  uint32_t* x =(uint32_t*)0x81d90004;
   _Context* ret = NULL;
   switch (e.event) {
     case _EVENT_TLBL: __am_tlb_refill();ret=c;break;
     case _EVENT_SYSCALL: do_syscall(c);break;
-    case _EVENT_YIELD: ret = schedule(c);printf("schedule before %x after %x as_addr is %x\n",c,ret,ret->as);break;
+    case _EVENT_YIELD: ret = schedule(c);printf("schedule before %x after %x as_addr is %x and as->ptr is %X\n",c,ret,ret->as,*x);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
