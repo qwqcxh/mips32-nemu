2653a53c47ce07b2e8c98f612bda3fb8ee69b5f3
tracer-ics2019 <tracer@njuics.org>
2019-12-22 13:56:29 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:56:29 up  4:14,  1 user,  load average: 0.27, 0.24, 0.28 d3f7231910449fd4a18f69f7231162e25c2945e7

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -53,6 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
+  _protect(&pcb->as);
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
   ((void(*)())entry) ();
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(&pcb[1],"/bin/init");
+  naive_uload(&pcb_boot,"/bin/init");
 }
 
 _Context* schedule(_Context *prev) {
