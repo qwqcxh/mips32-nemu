c818edae356b105353fa1b4e5f92de1d8fd9cab4
tracer-ics2019 <tracer@njuics.org>
2019-12-22 15:38:04 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:38:04 up  5:55,  1 user,  load average: 0.58, 0.32, 0.30 2a627e276bbaa0f7976b9e5a2eb1f0726a3712d9

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ static _Context* do_event(_Event e, _Context* c) {
   _Context* ret = NULL;
   void* x = (void*)0x81d8f000;
   switch (e.event) {
-    case _EVENT_TLBL: c->epc-=4;__am_tlb_refill();ret=c;break;
+    case _EVENT_TLBL: __am_tlb_refill();ret=c;break;
     case _EVENT_SYSCALL: do_syscall(c);break;
     case _EVENT_YIELD: ret = schedule(c);printf("schedule before %x after %x and pcb[1].cp %x\n",c,ret,*(uint32_t*)x);break;
     default: panic("Unhandled event ID = %d", e.event);
