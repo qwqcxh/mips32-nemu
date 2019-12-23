eb559fdac38d243d964ef235ca40484d9ef1dec8
tracer-ics2019 <tracer@njuics.org>
2019-12-23 14:06:17 +0800

    >  compile U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:06:17 up  4:53,  1 user,  load average: 1.93, 1.42, 1.35 7244b2829f9b1b81eb5c46683c2b1ce2d56fea57

--- nemu/src/isa/mips32/mmu.c
+++ nemu/src/isa/mips32/mmu.c
@@ -37,7 +37,7 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
   uint32_t realaddr = va2pa(addr,false);
   if(realaddr == (uint32_t)-1) printf("%s :realaddr of addr %x miss\n",__FUNCTION__,addr);//debug
   else if(addr < 0x80000000 ) {
-    printf("%s :realaddr of addr %x is %x\n",__FUNCTION__,addr,realaddr);//debug
+    printf("%s :realaddr of addr %x is %x value is %x\n",__FUNCTION__,addr,realaddr,paddr_read(realaddr,len));//debug
     printf("****************END*****************\n");//debug
   }
   if(tlbmiss) {tlbmiss = false; return 0;}
@@ -48,7 +48,7 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   uint32_t realaddr = va2pa(addr, true);
   if(realaddr == (uint32_t)-1) printf("%s :realaddr of addr %x miss\n",__FUNCTION__,addr);//debug
   else if(addr < 0x80000000 ) {
-    printf("%s :realaddr of addr %x is %x\n",__FUNCTION__,addr,realaddr);//debug
+    printf("%s :realaddr of addr %x is %x write_data %x\n",__FUNCTION__,addr,realaddr,data);//debug
     printf("****************END*****************\n");//debug
   }
   if(tlbmiss) {tlbmiss = false; return ;}
