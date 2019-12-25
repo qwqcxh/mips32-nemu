0390c5f8f4a10b0175887fbef5edb375733b2528
tracer-ics2019 <tracer@njuics.org>
2019-12-25 23:04:13 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:04:13 up 14:05,  1 user,  load average: 0.62, 0.45, 0.37 17daf70c4a518be6632579f0c5d87ca63997678f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,15 +19,15 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
     case SYS_yield: _yield();c->GPRx=0;break;
-    case SYS_brk: printf("sys_brk: cpu.epc %x\n",c->epc); c->GPRx = mm_brk(c->GPR2); break;
+    case SYS_brk: /*printf("sys_brk: cpu.epc %x\n",c->epc);*/ c->GPRx = mm_brk(c->GPR2); break;
       // program_break=(char*)c->GPR2; c->GPRx=0; break;
-    case SYS_exit:printf("sys_exit: cpu.epc %x\n",c->epc); naive_uload(NULL,"/bin/init"); break;
-    case SYS_open:printf("sys_open: cpu.epc %x\n",c->epc); c->GPRx=fs_open((char*)c->GPR2,c->GPR3,c->GPR4);break;
-    case SYS_read:printf("sys_read: cpu.epc %x\n",c->epc); c->GPRx=fs_read(c->GPR2,(void*)c->GPR3,c->GPR4);break;
-    case SYS_close:printf("sys_close: cpu.epc %x\n",c->epc); c->GPRx=fs_close(c->GPR2);break;
-    case SYS_lseek:printf("sys_lseek: cpu.epc %x\n",c->epc); c->GPRx=fs_lseek(c->GPR2,c->GPR3,c->GPR4);break;
+    case SYS_exit:/*printf("sys_exit: cpu.epc %x\n",c->epc);*/ assert(0);naive_uload(NULL,"/bin/init"); break;
+    case SYS_open:/*printf("sys_open: cpu.epc %x\n",c->epc);*/ c->GPRx=fs_open((char*)c->GPR2,c->GPR3,c->GPR4);break;
+    case SYS_read:/*printf("sys_read: cpu.epc %x\n",c->epc);*/ c->GPRx=fs_read(c->GPR2,(void*)c->GPR3,c->GPR4);break;
+    case SYS_close:/*printf("sys_close: cpu.epc %x\n",c->epc);*/ c->GPRx=fs_close(c->GPR2);break;
+    case SYS_lseek:/*printf("sys_lseek: cpu.epc %x\n",c->epc);*/ c->GPRx=fs_lseek(c->GPR2,c->GPR3,c->GPR4);break;
     case SYS_write:/*printf("sys_write: cpu.epc %x\n",c->epc);*/ c->GPRx = fs_write(c->GPR2,(void*)c->GPR3,c->GPR4);break;
-    case SYS_execve:printf("sys_execve: cpu.epc %x\n",c->epc); naive_uload(NULL,(char*)c->GPR2);break;
+    case SYS_execve:/*printf("sys_execve: cpu.epc %x\n",c->epc);*/assert(0); naive_uload(NULL,(char*)c->GPR2);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
