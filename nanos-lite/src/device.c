7633678e028d5d7d77a228bd18535658fac5a2a2
tracer-ics2019 <tracer@njuics.org>
2019-12-25 16:59:19 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:59:19 up  8:00,  1 user,  load average: 1.26, 1.32, 1.48 e86ea861d8462f2fc65cbd629b0ec5484621900

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,8 +3,8 @@
 #include "fs.h"
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  printf("%s\n",__FUNCTION__);//debug
-  _yield();
+  // printf("%s\n",__FUNCTION__);//debug
+  // _yield();
   for(int i=0;i<len;i++) _putc(((char*)buf)[i]);
   return len;
 }
@@ -18,8 +18,8 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-    printf("%s\n",__FUNCTION__);//debug
-    _yield();
+    // printf("%s\n",__FUNCTION__);//debug
+    // _yield();
     int key = read_key();
     int down = 0;
     if (key & 0x8000) {
@@ -43,8 +43,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  printf("%s\n",__FUNCTION__);//debug
-  _yield();
+  // printf("%s\n",__FUNCTION__);//debug
+  // _yield();
   int width  = screen_width();
   offset/=4;
   int x = offset % width;
