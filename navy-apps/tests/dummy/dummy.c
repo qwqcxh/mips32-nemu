8526c2120c4e30023f2867cde4477dc2bc84ae0a
tracer-ics2019 <tracer@njuics.org>
2019-12-25 20:51:54 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:51:54 up 11:52,  1 user,  load average: 0.45, 0.32, 0.36 139bb98843d7a06233d2d270c9694297edd2b1f9

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,8 +1,13 @@
 #include <stdint.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  return _syscall_(SYS_yield, 0, 0, 0);
+  // return _syscall_(SYS_yield, 0, 0, 0);
+  char* p=(char*)malloc(20*sizeof(char));
+  p[0]='a';p[1]='b';p[2]='c';p[3]='\0';
+  printf("addr of p: %x and val %s\n",p,p);
+  free(p);
 }
