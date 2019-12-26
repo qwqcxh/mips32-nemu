13bbe6d1ab9fe26a0656d93921c03cc78c269ff0
tracer-ics2019 <tracer@njuics.org>
2019-12-26 21:11:32 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:11:32 up 1 day, 12:12,  1 user,  load average: 0.87, 0.83, 0.82 599aff6d97eafd20b6598e8fcf60df9474794814

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
   _Context* ret = NULL;
   switch (e.event) {
-    case _EVENT_IRQ_TIMER:_yield();ret=c;break;
+    case _EVENT_IRQ_TIMER: printf("TIMER INTERRUPT!!!\n");_yield();ret=c;break;
     case _EVENT_TLBL: __am_tlb_refill();ret=c;break;//printf("TLB REFILL: cpu.epc %x\n",c->epc);break;
     case _EVENT_SYSCALL: ret = do_syscall(c);break;//printf("syscall finished and cpu.epc is %x\n",ret->epc);break;
     case _EVENT_YIELD: ret = schedule(c);__am_tlb_clear();break;//printf("schedule before %x after %x and cpu.epc %x\n",c,ret,ret->epc);break;
