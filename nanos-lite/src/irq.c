4dd049b524cb581fd56051e331fdcdbcf401ac18
tracer-ics2019 <tracer@njuics.org>
2019-12-26 14:37:30 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:37:30 up 1 day,  5:38,  1 user,  load average: 0.69, 0.51, 0.42 40923c56e3de67b9caa2f63b39db326b8a22529

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,9 +5,9 @@ extern _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
   _Context* ret = NULL;
   switch (e.event) {
-    case _EVENT_TLBL: __am_tlb_refill();ret=c;break;//printf("TLB REFILL: cpu.epc %x\n",c->epc);break;
-    case _EVENT_SYSCALL: ret = do_syscall(c);break;//printf("syscall finished and cpu.epc is %x\n",ret->epc);break;
-    case _EVENT_YIELD: ret = schedule(c);__am_tlb_clear();break;//printf("schedule before %x after %x and cpu.epc %x\n",c,ret,ret->epc);break;
+    case _EVENT_TLBL: __am_tlb_refill();ret=c;printf("TLB REFILL: cpu.epc %x\n",c->epc);break;
+    case _EVENT_SYSCALL: ret = do_syscall(c);printf("syscall finished and cpu.epc is %x\n",ret->epc);break;
+    case _EVENT_YIELD: ret = schedule(c);__am_tlb_clear();printf("schedule before %x after %x and cpu.epc %x\n",c,ret,ret->epc);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
