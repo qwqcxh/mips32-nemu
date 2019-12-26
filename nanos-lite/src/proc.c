7f3bc9ed0d48c02009ddd6fd7912847ac134ffe0
tracer-ics2019 <tracer@njuics.org>
2019-12-26 23:10:02 +0800

    >  run U201614789 marlin Linux marlin-K84HR 4.15.0-72-generic #81-Ubuntu SMP Tue Nov 26 12:20:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:10:03 up 1 day, 14:10,  1 user,  load average: 0.65, 0.53, 0.58 b2a681915f6e95cfd3735692e5bea63d288890c0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ extern void context_kload(PCB *pcb, void *entry);
 extern void context_uload(PCB *pcb, const char *filename) ;
 void init_proc() {
   // context_kload(&pcb[0], (void *)hello_fun);
-  context_uload(&pcb[0],"/bin/hello");
+  context_uload(&pcb[0],"/bin/slider");
   context_uload(&pcb[1], "/bin/pal");
   switch_boot_pcb();
   Log("Initializing processes...");
